26d92d41dfa19a1feae458aaf1a9ddf2bdf7e9a7
Marina <ejiwe83@ya.ru>
2024-02-14 21:48:16 +0100

    task

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.print("enter the number" + "\n");
+        Scanner scNumber = new Scanner(System.in);
+        int number = scNumber.nextInt();
+        if (number == 7) System.out.print("Привет" + "\n");
+
+        System.out.print("enter name" + "\n");
+        Scanner scName = new Scanner(System.in);
+        String name = scName.nextLine();
+        if (name.equals("Вячеслав") || name.equals("вячеслав")) System.out.print("Привет, " + name);
+        else System.out.print("Нет такого имени");
         }
     }
-}
